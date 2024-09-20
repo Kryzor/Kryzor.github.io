1e1181c200e9c0e911c5c29db914869626bfaf67
Kryzor <116125485+Kryzor@users.noreply.github.com>
2024-09-20 10:43:33 -0600

    Update sketch.js

--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -1,16 +1,72 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Interactive Scene
+// Katos Booth
+// September 2024
 
+
+let screenIsVisible = true;
+let highlighted = false;
+let pressed = false;
+let x = 128;
+let y = 128;
+let buttonWidth = 500;
+let buttonHeight = 50;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
 }
-
 function draw() {
-  background(220);
-  circle(mouseX,mouseY,30);
+  createCanvas(windowWidth,windowHeight);
+  if (screenIsVisible){
+    startScreen();
+    button();
+  }
+  else {
+    gameScreen();
+  } 
+}
+function startScreen(){
+  background(56,120,200);
+}
+function gameScreen(){
+  background(150,60,80);
+}
+function button(){
+  buttonIsHighlighted();
+  buttonIsPressed();
+  x = (windowWidth/2)-buttonWidth/2;
+  y = (windowHeight/2)-buttonHeight/2;
+  if (highlighted){
+    fill(150);
+    strokeWeight(4);
+    stroke(255,255,0);
+  }
+  else if (pressed){
+    fill(100);
+    stroke(255,255,0);
+  }
+  else {
+    strokeWeight(2);
+    fill(150);
+    stroke(255);
+  }
+  rect(x, y, buttonWidth, buttonHeight);
+  textAlign(CENTER);
+  textSize(20);
+  text('Start the uhhhhhhhh game', x, y+buttonHeight/3, buttonWidth, buttonHeight);
+}
+function buttonIsPressed() {
+  if (mouseX > x && mouseX < x + buttonWidth && mouseY > y && mouseY < y + buttonHeight && mouseIsPressed) {
+    pressed = true;
+    screenIsVisible = false;
+  }
+  else{
+    pressed = false;
+  }
+}
+function buttonIsHighlighted() {
+  if (mouseX > x && mouseX < x + buttonWidth && mouseY > y && mouseY < y + buttonHeight && !mouseIsPressed) {
+    highlighted = true;
+  }
+  else {
+    highlighted = false
+  }
 }
