83ac420661e3fa61a526c09c21963c1f681bc74b
Kryzor <116125485+Kryzor@users.noreply.github.com>
2024-10-11 10:06:37 -0600

    Updated project
    touched up start menu and started to add the ball stuff hoping to add physics to it at some point

--- arrays_and_object_notation/sketch.js
+++ arrays_and_object_notation/sketch.js
@@ -6,8 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let screenState = 0;
+let ballsArray = [];
 
-const button = {
+let button = {
   w: 500,
   h: 100,
   x: 0,
@@ -19,17 +20,15 @@ const button = {
 };
 
 
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  //sets button positions to be effected by the windows size
-  button.x = (windowWidth - button.w)/2;
-  button.y = (windowHeight - button.h)/2;
-}
+function setup() {}
 
 function draw() {
-  background(220);
+
+  //updates the window variables as the window is messed with to prevent weird visual issues
+  createCanvas(windowWidth, windowHeight);
+  button.x = (windowWidth - button.w)/2;
+  button.y = (windowHeight - button.h)/2;
+
   screenController();
 }
 
@@ -38,25 +37,30 @@ function screenController(){
   if (screenState === 0){
     mainScreen();
   }
-  else if (screenState === 1){
+  if (screenState === 1){
     gameScreen();
   }
 }
 
+//the main screen that shows up upon starting the project
 function mainScreen(){
+  background(100,160,200);
   setupButton();
 }
 
+//the game screen that shows when the main menu button is pressed
 function gameScreen() {
-
+  background(220);
+  createBall(random(windowWidth), random(windowHeight));
 }
 
+//the setup for the start menu button
 function setupButton(){
   fill(30,220,80);
   if (mouseX > button.x && button.w && mouseX < button.x + button.w && mouseY > button.y && button.h && mouseY < button.y + button.h){
     stroke(10,170,10);
     strokeWeight(5);
-    if (mousePressed){
+    if (mouseIsPressed){
       screenState = 1;
     }
   }
@@ -65,4 +69,28 @@ function setupButton(){
     strokeWeight(1);
   }
   rect(button.x, button.y, button.w, button.h, button.tl, button.tr);
+  textSize(40);
+  textAlign(CENTER);
+  fill(240);
+  text("Start the ball playground", button.x + button.w/2, button.y + button.h/1.6);
+}
+
+//creates the ball
+function createBall(ballX, ballY){
+  noStroke();
+  fill(ball.r,ball.g,ball.b);
+  circle(theBall.x, theBall.y, theBall.radius*2);
+  let ball = {
+    x: ballX,
+    y: ballY,
+    radius: 10,
+    r: random(255),
+    g: random(255),
+    b: random(255),
+  };
+  ballsArray.push(ball);
+}
+
+function ballMovement(){
+
 }
